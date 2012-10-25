0ecc53e5a1b43e621f13a52b30f2324ac9d1817e
Rob Percival <rob.percival87@gmail.com>
2012-10-26 00:58:49 +0100

    Prototype Windows 8 tile design

---
+++ index.php
@@ -0,0 +1,80 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+	<head>
+		<title>Charity Auction</title>
+		<link href="~/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+		<meta name="viewport" content="width=device-width" />
+		<link href="css/normalize.css" rel="stylesheet" type="css/stylesheet" />
+		<link href="css/site.css" rel="stylesheet" type="css/stylesheet" />
+	</head>
+	<body>
+		<div class="header">
+			<div class="title">
+				<h1>Title</h1>
+			</div>
+		</div>
+		
+		<div id="body">
+			<div class="tile-group">
+				<h2>Lots</h2>
+
+				<div class="tile featured">
+					<img src="images/Placeholder.png" alt="Coming Soon..." />
+					<div>
+						<h3>Item 1</h3>
+						<p>Description...</p>
+					</div>
+				</div>
+				<div class="tile">
+					<img src="images/Placeholder.png" alt="Coming Soon..." />
+					<div><h3>Item 2</h3></div>
+				</div>
+				<div class="tile">
+					<img src="images/Placeholder.png" alt="Coming Soon..." />
+					<div><h3>Item 3</h3></div>
+				</div>
+				<div class="tile">
+					<img src="images/Placeholder.png" alt="Coming Soon..." />
+					<div><h3>Item 4</h3></div>
+				</div>
+				<div class="tile">
+					<img src="images/Placeholder.png" alt="Coming Soon..." />
+					<div><h3>Item 5</h3></div>
+				</div>
+				<div class="tile">
+					<img src="images/Placeholder.png" alt="Coming Soon..." />
+					<div><h3>More Items...</h3></div>
+				</div>
+			</div>
+
+			<div class="tile-group">
+				<h2>Donate</h2>
+
+				<div class="tile featured">
+					<img src="images/Placeholder.png" alt="Coming Soon..." />
+				</div>
+				<div class="tile">
+					<img src="images/Placeholder.png" alt="Coming Soon..." />
+				</div>
+				<div class="tile">
+					<img src="images/Placeholder.png" alt="Coming Soon..." />
+				</div>
+			</div>
+
+			<div class="tile-group">
+				<h2>About</h2>
+
+				<div class="tile featured">
+					<img src="images/Placeholder.png" alt="Coming Soon..." />
+				</div>
+				<div class="tile tall">
+					<img src="images/Placeholder.png" alt="Coming Soon..." />
+				</div>
+			</div>
+		</div>
+		
+		<div class="footer">
+			
+		</div>
+	</body>
+</html>
